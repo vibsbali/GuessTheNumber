e7d5707d226162acaede5628e1115445729d62eb
Vaibhav Bali <vibsbali@gmail.com>
2016-05-10 15:18:35 +1000

    Added module and controller along with updating index.html file

---
+++ GuessTheNumber/app/guessTheNumberController.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+(function () {
+    angular.module("guessTheNumber").controller("guessTheNumberController", function () {
+        var vm = this;
+
+        vm.initializeGame = function () {
+            vm.noOfTries = 0;
+            vm.original = Math.floor((Math.random() * 1000) + 1);
+            vm.guess = null;
+            vm.deviation = null;
+        };
+
+        vm.verifyGuess = function () {
+            vm.deviation = vm.original - vm.guess;
+            ++vm.noOfTries;
+        };
+        vm.initializeGame();
+    });
+}())
